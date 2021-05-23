456e33a04d126bcf0cf9463997843a5e28f150ad
Donna <skies09@hotmail.com>
2021-05-23 18:33:02 +0100

    initial commit

---
+++ accounts/url_reset.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+
+urlpatterns = [
+    url(r'^$', PasswordResetView.as_view(),{'post_reset_redirect': reverse_lazy('password_reset_done')}, name='reset_password'),
+    url(r'^done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(),
+        {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
+    url('^complete/$', PasswordResetCompleteView.as_view(), name='password_reset_complete')
+]
+
+
+
